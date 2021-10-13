b96c9a76e17906cf9265f1d889651249d9dc2d3a
Snowballe <snacki3andmr.5@gmail.com>
2021-10-13 16:54:18 +0200

    fix index

--- src/index.js
+++ src/index.js
@@ -1,29 +1,55 @@
 const client = require("./client/client.js");
-require("./services/weather.js");
+const w_token = process.env.WEATHER_TOKEN;
+var axios = require('axios');
+
 
 const prefixCmd = '!';
 
-client.on ("ready", () => {
+client.once("ready", () => {
     console.log("🌤 Weather Bot Online ☁");
 });
 
 client.on("message", msg => {
 
     if(!msg.content.startsWith(prefixCmd) || msg.author.bot) return
-   
-    const args = msg.content.trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-    if (command === 'help') {
-        msg.reply('Bien le bonjour ! Ce bot discord est là pour indiquer la météo ! Lancez !weather [Votre ville] pour trouver la météo de la ville en question !');
-      }
-    
-    if (command === "weather") {
-        
-       
-        //var montruc = JSON.parse(country);
 
-        msg.reply("Voici la météo du jour à "+args);
+    const args = msg.content.slice(prefixCmd.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === "dailymeteo") {
+        msg.messageCreate("Voici la météo du jour !");
+        console.log(args);
+    }
+    if(command==="temps"){
+        msg.messageCreate();
+
+    }
+    if(command==="t"){
+     const villeNormalise=args[0].toString().toLowerCase().replace(/^\w/, (c) => c.toUpperCase());//le truc en chinois de replace à uppercase c'est pour faire un capitalize
+    
+     axios
+        .get('http://api.openweathermap.org/data/2.5/weather',{
+            params:{
+                q:villeNormalise,
+                appid:w_token
+            }
+        })
+        .then(response=>{
+            console.log(apiData.data);
+        
+        });
+
+
+     
+     msg.reply(`Voici le temps qu'il fait à ${villeNormalise} :`);
+     //  http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=
     }
 
 });
+
+// const userAction = async () => {
+//     const response = await fetch('https://openweathermap.org/api');
+//     const myJson = await response.json(); //extract JSON from the http response  
+//   }
+
 
