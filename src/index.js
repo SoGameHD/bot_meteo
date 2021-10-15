ca5e6db8950f701988347ae7be4ec84650ae5be3
Snowballe <62845660+Snowballe@users.noreply.github.com>
2021-10-15 18:45:15 +0200

    Fixed gardage code from earlier

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const client = require("./client/client.js");
 const w_token = process.env.WEATHER_TOKEN;
 const weather = require('openweather-apis');
+const windDirection=require('./utils/windDirection.json')
+const ptiteBlague=require('./utils/humor.json')
+
 
 const prefixCmd = '!';
 
@@ -36,108 +39,20 @@ client.on("message", msg => {
          if(error) console.error(error);
 
         const vent=resApi['wind']['speed'];
-        const directionVent=resApi['wind']['deg'];
-
+        var directionVent=resApi['wind']['deg'];
+        
+        const directionVentNormalise = Math.round(directionVent/10)*10;
+        const ventHumour=vent|0;//Double to int en gros
+        //${ptiteBlague[ventHumour]} à copier dans la phrase pour donner de l'humour au bot
         msg.reply(`Voici le temps qu'il fait à ${villeNormalise} :`);
-        console.log(directionVent)
 
-        //====================== TODO : rendre le code d'en dessous lisible, et pas un truc qui arrache les yeux (Faire un JSON qui pourra pick en fonction de l'angle ?)======
-        switch (directionVent) {
-            case 0:
-         msg.reply(`Niveau vent, on est à ${vent} noeuds, et on est plus orienté plein Est`)
-               
-                break;
-            case 30:
-            msg.reply(`Niveau vent, on est à ${vent} noeuds, et on est plus orienté Est/Nord-Est`)
-                      
-             break;
-            
-             case 60:
-            msg.reply(`Niveau vent, on est à ${vent} noeuds, et on est plus orienté Nord/Nord-Est`)
-                      
-             break;
-             case 120:
-            msg.reply(`Niveau vent, on est à ${vent} noeuds, et on est plus orienté Nord/Nord-Ouest`)
-                      
-             break;
-             case 150:
-            msg.reply(`Niveau vent, on est à ${vent} noeuds, et on est plus orienté Ouest/Nord-Ouest`)
-                      
-             break;
-             case 180:
-            msg.reply(`Niveau vent, on est à ${vent} noeuds, et on est plus orienté plein Ouest`)
-                      
-             break;
-             case 210:
-            msg.reply(`Niveau vent, on est à ${vent} noeuds, et on est plus orienté Ouest/Sud-Ouest`)
-                      
-             break;
-             
-             case 240:
-             msg.reply(`Niveau vent, on est à ${vent} noeuds, et on est plus orienté Sud/Sud-Ouest`)
-                       
-              break;
-              case 270:
-            msg.reply(`Niveau vent, on est à ${vent} noeuds, et on est plus orienté plein Sud`)
-                      
-             break;
-             case 300:
-                msg.reply(`Niveau vent, on est à ${vent} noeuds, et on est plus orienté Sud/Sud-Est`)
-                          
-                 break;
-            case 330:
-                msg.reply(`Niveau vent, on est à ${vent} noeuds, et on est plus orienté Est/Sud-Est`)
-                          
-            break;
-            case 360:
-                msg.reply(`Niveau vent, on est à ${vent} noeuds, et on est plus orienté plein Est`)
-                          
-                 break;
-            default:
-                break;
-        }
-         
-         if (directionVent<30 && directionVent>0||directionVent>330&&directionVent<360) {//Vent orienté Est
-         msg.reply(`Niveau vent, on est à ${vent} noeuds, et on est plus orienté Est`)
-             
-         } else if(directionVent>30&&directionVent<60) {
-            msg.reply(`Niveau vent, on est à ${vent} noeuds, et on est plus orienté Nord-Est.`)
-         
-        } else if(directionVent>60&&directionVent<120){//Nord
-            
-            msg.reply(`Niveau vent, on est à ${vent} noeuds, et on est plus orienté Nord`)
-
-         }else if(directionVent>120&&directionVent<150){
-            
-            msg.reply(`Niveau vent, on est à ${vent} noeuds, et on est plus orienté Nord-Ouest`)
-         
-        }else if(directionVent>150&&directionVent<210){
-            msg.reply(`Niveau vent, on est à ${vent} noeuds, et on est plus orienté Ouest`)
-        
-        }else if(directionVent>210&&directionVent<240){
-            msg.reply(`Niveau vent, on est à ${vent} noeuds, et on est plus orienté Sud-Ouest`)
-        
-        }else if(directionVent>240&&directionVent<300){
-            msg.reply(`Niveau vent, on est à ${vent} noeuds, et on est plus orienté Sud`)
-        
-        }else if(directionVent>300&&directionVent<330){
-            msg.reply(`Niveau vent, on est à ${vent} noeuds, et on est plus orienté Sud-Est`)
-          //=====================================================================================================
-         }
+        msg.reply(`Niveau vent, on est à ${vent} noeuds, avec une orientation **${windDirection[directionVentNormalise]}**`);
 
      })
-        
-
-
-     
      
     }
 
 });
 
-// const userAction = async () => {
-//     const response = await fetch('https://openweathermap.org/api');
-//     const myJson = await response.json(); //extract JSON from the http response  
-//   }
 
 
