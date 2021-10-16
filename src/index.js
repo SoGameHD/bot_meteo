24c329d5e91ec221f6f5029888d4f7c462252149
Snowballe <62845660+Snowballe@users.noreply.github.com>
2021-10-16 23:26:07 +0200

    trying to import images

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const client = require("./client/client.js");
 const w_token = process.env.WEATHER_TOKEN;
 const weather = require('openweather-apis');
 const windDirection=require('./utils/windDirection.json')
-const ptiteBlague=require('./utils/humor.json')
+const presentation=require('./services/imageSearch.js')
+const pays=require('./utils/countries.json');
 
 
 const prefixCmd = '!';
@@ -19,7 +20,7 @@ client.on("message", msg => {
     const command = args.shift().toLowerCase();
 
     if (command === "help") {
-        msg.messageCreate("Ce bot te dis la météo du jour, où tu veux ! Pour me lancer, fait !t [ta ville] \nSi jamais tu ne tombe pas dessus du premier coup, précise le pays !");
+        msg.reply("Ce bot te dis la météo du jour, où tu veux ! Pour me lancer, fait !t [ta ville] \nSi jamais tu ne tombe pas dessus du premier coup, précise le pays !");
         console.log(args);
     }
     if(command==="temps"){
@@ -36,17 +37,23 @@ client.on("message", msg => {
      weather.setCity(villeNormalise);
 
      weather.getAllWeather(function(error, resApi){
-         if(error) console.error(error);
+         
+        if(error) console.error(error);
 
-        const vent=resApi['wind']['speed'];
+
+        console.log(resApi);
+        const vent =resApi['wind']['speed'];
+        const rafale=resApi['wind']['gust'];
         var directionVent=resApi['wind']['deg'];
         
         const directionVentNormalise = Math.round(directionVent/10)*10;
         const ventHumour=vent|0;//Double to int en gros
         //${ptiteBlague[ventHumour]} à copier dans la phrase pour donner de l'humour au bot
-        msg.reply(`Voici le temps qu'il fait à ${villeNormalise} :`);
-
+        
+        
+        msg.reply(`Voici le temps qu'il fait à ${villeNormalise}, ${pays[resApi['sys']['country']]} :`);
         msg.reply(`Niveau vent, on est à ${vent} noeuds, avec une orientation **${windDirection[directionVentNormalise]}**`);
+        //msg.reply(presentation.image(msg, villeNormalise));
 
      })
      
